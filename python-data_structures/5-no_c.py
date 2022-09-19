a7d1d71741ac280dcd01a384afb1b5bcb113ffc4
Sebastian <sebaslopez.2002@gmail.com>
2022-09-19 17:40:20 -0500

    5

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -4,7 +4,7 @@
 def no_c(my_string: str):
     new_string = ''
     for c in my_string:
-        if c != 'c' or c != 'C':
+        if c != 'c' and c != 'C':
             new_string += c
 
     return new_string
