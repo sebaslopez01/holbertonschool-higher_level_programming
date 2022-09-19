3b100b886c48e69f730c37e8c3c81febf87041c1
Sebastian <sebaslopez.2002@gmail.com>
2022-09-19 17:52:39 -0500

    6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,6 +2,10 @@
 
 
 def print_matrix_integer(matrix=[[]]):
+    if len(matrix) == 0:
+        print()
+        return
+
     for i in matrix:
         for l, j in enumerate(i):
             if l != len(i) - 1:
