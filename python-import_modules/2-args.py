6a55924c61b665c102cc8021c0f063e58515cb80
Sebastian <sebaslopez.2002@gmail.com>
2022-09-19 16:03:02 -0500

    2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
         for i, arg in enumerate(sys.argv[1:], 1):
             print("{}: {}".format(i, arg))
     elif len(sys.argv) > 2:
-        print("{} argument:".format(len(sys.argv) - 1))
+        print("{} arguments:".format(len(sys.argv) - 1))
         for i, arg in enumerate(sys.argv[1:], 1):
             print("{}: {}".format(i, arg))
     else:
