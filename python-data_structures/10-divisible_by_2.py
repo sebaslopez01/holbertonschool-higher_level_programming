8d5c510ff6114923e3e0cae22dba80477ff5cac6
Sebastian <sebaslopez.2002@gmail.com>
2022-09-19 18:12:42 -0500

    10

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -7,6 +7,7 @@ def divisible_by_2(my_list=[]):
     for n in my_list:
         if n % 2 == 0:
             new_list.append(True)
-        new_list.append(False)
+        else:
+            new_list.append(False)
 
     return new_list
