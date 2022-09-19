2abefbffcefcb6d10f7fd7209692eb068e091333
Sebastian <sebaslopez.2002@gmail.com>
2022-09-19 15:46:09 -0500

    0

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
+if __name__ == "__main__":
+    from add_0 import add
 
-from add_0 import add
+    a = 1
+    b = 2
+    result = add(1, 2)
 
-a = 1
-b = 2
-result = add(1, 2)
-
-print("{} + {} = {}", a, b, result)
+    print("{} + {} = {}", a, b, result)
