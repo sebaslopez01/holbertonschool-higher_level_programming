37fbd86c091871b570f8942bd6b736d3c14992e9
Sebastian <sebaslopez.2002@gmail.com>
2022-09-26 09:16:21 -0500

    7

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
 def update_dictionary(a_dictionary: dict, key: str, value):
-    new_dict = a_dictionary.copy()
 
-    new_dict[key] = value
+    a_dictionary[key] = value
 
-    return new_dict
+    return a_dictionary
