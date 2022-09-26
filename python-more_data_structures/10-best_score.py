b7d15cdbb8ce089cff6bb5ad7bb385c6d55419e7
Sebastian <sebaslopez.2002@gmail.com>
2022-09-26 09:32:46 -0500

    10

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -4,7 +4,7 @@
 def best_score(a_dictionary: dict):
     try:
         max_score = max(a_dictionary.values())
-    except ValueError:
+    except (ValueError, AttributeError):
         return None
     result = next(filter(lambda x: x[1] == max_score, a_dictionary.items()))[0]
 
