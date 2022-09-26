4e33bf8a477c159bee736956cbf2e0ac9f23803a
Sebastian <sebaslopez.2002@gmail.com>
2022-09-26 11:33:22 -0500

    Square

---
+++ python-classes/1-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+""" Square class definition
+
+This module defines a Square class
+
+"""
+
+
+class Square:
+    """ Square class.
+
+    Square class definition
+
+    Attributes:
+        __size (int): Size of the square
+
+    """
+
+    def __init__(self, size):
+        """ __init__ method.
+
+        Initialization method
+
+        Args:
+            size (int): Size of the square
+        """
+        self.__size = size
