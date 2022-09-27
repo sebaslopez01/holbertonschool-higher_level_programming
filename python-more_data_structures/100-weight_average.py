f1f9c11f4b217b4fe24fbe625da67e9a477930d5
Sebastian <sebaslopez.2002@gmail.com>
2022-09-26 19:21:53 -0500

    100

--- python-more_data_structures/100-weight_average.py
+++ python-more_data_structures/100-weight_average.py
@@ -5,4 +5,5 @@ def weight_average(my_list=[]):
     if len(my_list) == 0:
         return 0
 
-    return sum(lst[0] * lst[1] for lst in my_list) / sum(map(lambda x: x[1], my_list))
+    return sum(lst[0] * lst[1] for lst in my_list) \
+        / sum(map(lambda x: x[1], my_list))
