e98766fb0098cf53c23a237f4129731bcc500f99
Sebastian <sebaslopez.2002@gmail.com>
2022-09-26 19:40:02 -0500

    100

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -6,7 +6,7 @@ def safe_print_integer_err(value):
     try:
         print('{:d}'.format(value))
     except (ValueError, TypeError) as e:
-        sys.stderr.write(f'Exception: {e}')
+        print(f'Exception: {e}', end='', file=sys.stderr)
         return False
 
     return True
