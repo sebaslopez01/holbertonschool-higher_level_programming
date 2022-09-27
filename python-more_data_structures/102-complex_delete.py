2f72ce240a81d3460989950fe9e7005a5e6702f6
Sebastian <sebaslopez.2002@gmail.com>
2022-09-26 19:31:47 -0500

    102

--- python-more_data_structures/102-complex_delete.py
+++ python-more_data_structures/102-complex_delete.py
@@ -5,3 +5,5 @@ def complex_delete(a_dictionary: dict, value):
     for item in a_dictionary.copy().items():
         if item[1] == value:
             a_dictionary.pop(item[0])
+
+    return a_dictionary
