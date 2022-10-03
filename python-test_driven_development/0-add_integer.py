6f94ad118bd85c38cf618e5f7235ec6d2ea6e8b6
Sebastian <sebaslopez.2002@gmail.com>
2022-10-03 10:43:27 -0500

    0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -20,8 +20,4 @@ def add_integer(a, b=98):
     if not isinstance(b, int) and not isinstance(b, float):
         raise TypeError('b must be an integer')
 
-    result = a + b
-    if result == float('inf') or result == -float('inf'):
-        return 89
-
     return int(a) + int(b)
