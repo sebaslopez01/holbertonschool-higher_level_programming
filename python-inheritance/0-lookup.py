0c234cafea90a454fbeb6fd076a1903918badd4e
Sebastian <sebaslopez.2002@gmail.com>
2022-10-10 16:34:28 -0500

    0

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -12,7 +12,7 @@ def lookup(obj: object):
     """ Get a list of available attributes and methods of
     an object
 
-    Args: 
+    Args:
         obj (:obj:`object`): Object to get the attributes
 
     Returns:
