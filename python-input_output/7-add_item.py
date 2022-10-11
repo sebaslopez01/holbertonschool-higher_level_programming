a9dac549c5e792652975f17b189bbcbe30943604
Sebastian <sebaslopez.2002@gmail.com>
2022-10-11 09:48:01 -0500

    7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -11,4 +11,6 @@ import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-save_to_json_file(sys.argv[1:], 'add_item.json')
+obj = load_from_json_file('add_item.json')
+obj += sys.argv[1:]
+save_to_json_file(obj, 'add_item.json')
