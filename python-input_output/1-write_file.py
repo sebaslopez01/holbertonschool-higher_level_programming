611252c8c2940a462b9d64b5c6f4e17b4c43c290
Sebastian <sebaslopez.2002@gmail.com>
2022-10-11 09:14:46 -0500

    1

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -19,4 +19,4 @@ def write_file(filename="", text=""):
         Number of characters written
     """
     with open(filename, 'w', encoding='utf-8') as f:
-        f.write(text)
+        return f.write(text)
