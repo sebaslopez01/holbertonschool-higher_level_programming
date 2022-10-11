d4cf652317a809cec0b5b457767362f10a6c47c7
Sebastian <sebaslopez.2002@gmail.com>
2022-10-11 09:13:59 -0500

    write file

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+
+This module defines a write_file function
+
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    Writes a string to a text file
+
+    Args:
+        filename (:obj:`str`, optional): Filename to write to
+        text (:obj:`str`, optional): Text to write to the file
+
+    Returns:
+        Number of characters written
+    """
+    with open(filename, 'w', encoding='utf-8') as f:
+        f.write(text)
