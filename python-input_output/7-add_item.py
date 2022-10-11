bf28f3c7518ed8ea7d3d8336e2e14ad2a1640544
Sebastian <sebaslopez.2002@gmail.com>
2022-10-11 09:59:08 -0500

    7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 
-import sys
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-
 """
 
 This module adds all arguments to a Python list and
 then save them
 
 """
+
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 def main():
