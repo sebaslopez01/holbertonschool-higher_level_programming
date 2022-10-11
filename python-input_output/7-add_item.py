4104a3ff7cd43105c800b9f14745983ddeea480b
Sebastian <sebaslopez.2002@gmail.com>
2022-10-11 09:50:31 -0500

    7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -11,6 +11,15 @@ import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-obj = load_from_json_file('add_item.json')
-obj += sys.argv[1:]
-save_to_json_file(obj, 'add_item.json')
+
+def main():
+    """
+    Adds all arguments to a Python list and
+    then save them
+    """
+    obj = load_from_json_file('add_item.json')
+    obj += sys.argv[1:]
+    save_to_json_file(obj, 'add_item.json')
+
+
+main()
