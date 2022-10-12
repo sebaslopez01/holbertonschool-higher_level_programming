5d1f45c4d4416fd486a67a81b1468fe73d75b117
Sebastian <sebaslopez.2002@gmail.com>
2022-10-12 10:57:47 -0500

    7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -11,6 +11,12 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 new_lst = []
-new_lst += load_from_json_file('add_item.json')
+
+try:
+    obj = load_from_json_file('add_item.json')
+    new_lst += obj
+except FileNotFoundError:
+    pass
+
 new_lst += sys.argv[1:]
 save_to_json_file(new_lst, 'add_item.json')
