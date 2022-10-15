82c83e5438f0770443b11d2fa339fefc52c42593
Sebastian <sebaslopez.2002@gmail.com>
2022-10-14 19:11:14 -0500

    append after

---
+++ python-input_output/100-append_after.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""
+
+This module defines a append_after function
+
+"""
+
+
+def append_after(filename='', search_string='', new_string=''):
+    """
+    Inserts a line of text to a file, after each line 
+    containing a specific string
+
+    Args:
+        filename (:obj:`str`, optional): Filename to insert string
+        search_string (:obj:`str`, optional): String to search for
+        new_string (:obj:`str`, optional): String to insert to the file
+    """
+
+    with open(filename, 'r', encoding='utf-8') as f:
+        data_lines = f.readlines()
+
+        for count, line in enumerate(data_lines):
+            if search_string in line:
+                data_lines.insert(count + 1, new_string)
+
+    with open(filename, 'w', encoding='utf-8') as f:
+        f.write(''.join(data_lines))
