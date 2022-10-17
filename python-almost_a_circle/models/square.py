d1866ff4deae93d242652d4b7fe382b437a5f11a
Sebastian <sebaslopez.2002@gmail.com>
2022-10-17 09:27:25 -0500

    square size

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -27,5 +27,14 @@ class Square(Rectangle):
         """
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value: int):
+        self.width = value
+        self.height = value
+
     def __str__(self):
         return f'[Square] ({self.id}) {self.x}/{self.y} - {self.width}'
