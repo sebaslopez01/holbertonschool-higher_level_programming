d48f4fb7fcb420b3772daaeb606a52ef844e1a1a
Sebastian <sebaslopez.2002@gmail.com>
2022-10-17 11:16:12 -0500

    tests

--- python-almost_a_circle/tests/test_models.py
+++ python-almost_a_circle/tests/test_models.py
@@ -13,8 +13,30 @@ from models.square import Square
 
 
 class TestModels(unittest.TestCase):
+    def test_assigning_automatically_id(self):
+        self.assertEqual(Base().id, 1)
+
+    def test_assigning_automatically_id_plus_one(self):
+        first_ins = Base()
+        self.assertEqual(Base().id, 2)
+
     def test_save_id(self):
         self.assertEqual(Base(89).id, 89)
 
-    def test_to_json_string(self):
+    def test_to_json_string_none(self):
         self.assertEqual(Base.to_json_string(None), '[]')
+
+    def test_to_json_string_empty(self):
+        self.assertEqual(Base.to_json_string([]), '[]')
+
+    def test_to_json_string_exists(self):
+        self.assertEqual(Base.to_json_string([{'id': 12}], '[{"id": 12}]'))
+
+    def test_from_json_string_none(self):
+        self.assertEqual(Base.from_json_string(None), [])
+
+    def test_from_json_string_empty(self):
+        self.assertEqual(Base.from_json_string("[]"), [])
+
+    def test_from_json_string_exists(self):
+        self.assertEqual(Base.from_json_string('[{ "id": 89 }]', [{'id': 89}]))
