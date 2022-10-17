6d5aa67ccecebe0568f24fc27b68e28749cf5581
Sebastian <sebaslopez.2002@gmail.com>
2022-10-17 08:50:14 -0500

    rectangle better

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -27,6 +27,23 @@ class Rectangle(Base):
             id (:obj:`int`): id of the instance
         """
         super().__init__(id)
+        if type(width) is not int:
+            raise TypeError('width must be an integer')
+        if width <= 0:
+            raise ValueError('width must be > 0')
+        if type(height) is not int:
+            raise TypeError('height must be an integer')
+        if height <= 0:
+            raise ValueError('height must be > 0')
+        if type(x) is not int:
+            raise TypeError('x must be an integer')
+        if x < 0:
+            raise ValueError('x must be >= 0')
+        if type(y) is not int:
+            raise TypeError('y must be an integer')
+        if y < 0:
+            raise ValueError('y must be >= 0')
+
         self.__width = width
         self.__height = height
         self.__x = x
@@ -38,6 +55,11 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value: int):
+        if type(value) is not int:
+            raise TypeError('width must be an integer')
+        if value <= 0:
+            raise ValueError('width must be > 0')
+
         self.__width = value
 
     @property
@@ -46,6 +68,11 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value: int):
+        if type(value) is not int:
+            raise TypeError('height must be an integer')
+        if value <= 0:
+            raise ValueError('height must be > 0')
+
         self.__height = value
 
     @property
@@ -54,6 +81,11 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value: int):
+        if type(value) is not int:
+            raise TypeError('x must be an integer')
+        if value < 0:
+            raise ValueError('x must be >= 0')
+
         self.__x = value
 
     @property
@@ -62,4 +94,9 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
+        if type(value) is not int:
+            raise TypeError('y must be an integer')
+        if value < 0:
+            raise ValueError('y must be >= 0')
+
         self.__y = value
