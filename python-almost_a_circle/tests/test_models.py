8aae75344ec7838337b9cd3b6be44a1ad4c64c7e
Sebastian <sebaslopez.2002@gmail.com>
2022-10-17 13:31:29 -0500

    test

--- python-almost_a_circle/tests/test_models.py
+++ python-almost_a_circle/tests/test_models.py
@@ -271,6 +271,6 @@ class TestRectangle(unittest.TestCase):
     def test_rectangle_load_from_file_not_exists(self):
         self.assertEqual(Rectangle.load_from_file(), [])
 
-    def test_rectangle_load_from_file_not_exists(self):
+    def test_rectangle_load_from_file_exists(self):
         lst = Rectangle.load_from_file()
         print(lst)
