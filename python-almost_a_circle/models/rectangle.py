aaa505ba4ca8abd8db74e49d2fbb8d85847963f1
Sebastian <sebaslopez.2002@gmail.com>
2022-10-17 08:33:08 -0500

    rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -63,13 +63,3 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         self.__y = value
-
-
-r1 = Rectangle(10, 2)
-print(r1.id)
-
-r2 = Rectangle(2, 10)
-print(r2.id)
-
-r3 = Rectangle(10, 2, 0, 0, 12)
-print(r3.id)
