e10bffa832af32b2ec418340b288695f9d2ab041
Sebastian <sebaslopez.2002@gmail.com>
2022-11-10 08:24:16 -0800

    goto to utf8

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,5 @@
 -- Go to UTF8
+USE hbtn_0c_0;
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE second_table CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE second_table MODIFY name CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
