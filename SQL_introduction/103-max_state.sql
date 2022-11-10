0504409c8ae4552f7d725814f42195149a0aa03f
Sebastian <sebaslopez.2002@gmail.com>
2022-11-10 08:37:46 -0800

    temperatures 2

--- SQL_introduction/103-max_state.sql
+++ SQL_introduction/103-max_state.sql
@@ -1,2 +1,2 @@
 -- Temperatures 2
-SELECT state, MAX(value) AS 'max_temp' FROM temperatures ORDER BY state DESC;
+SELECT state, MAX(value) AS 'max_temp' FROM temperatures GROUP BY state ORDER BY state DESC;
