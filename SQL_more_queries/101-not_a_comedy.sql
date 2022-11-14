4479ba6e85ab0c4764b475481220374175863f5f
Sebastian <sebaslopez.2002@gmail.com>
2022-11-14 12:06:39 -0800

    not comedy

--- SQL_more_queries/101-not_a_comedy.sql
+++ SQL_more_queries/101-not_a_comedy.sql
@@ -1,2 +1,2 @@
 -- No comedy
-SELECT DISTINCT tv_shows.title FROM tv_show_genres LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id WHERE show_id NOT IN (SELECT tv_shows.id FROM tv_show_genres LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id WHERE tv_genres.name = 'Comedy') ORDER BY tv_shows.title;
+SELECT DISTINCT tv_shows.title FROM tv_show_genres RIGHT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id WHERE show_id NOT IN (SELECT tv_shows.id FROM tv_show_genres RIGHT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id RIGHT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id WHERE tv_genres.name = 'Comedy') ORDER BY tv_shows.title;
