5bb57ef952aa085641b1d21760f3f9568cbaf78c
Sebastian <sebaslopez.2002@gmail.com>
2022-11-14 08:41:54 -0800

    genre id by show

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,2 +1,2 @@
 -- Genre id by show
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_show_genres LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id WHERE tv_show_genres.genre_id >= 1 ORDER BY tv_shows.title AND tv_show_genres.genre_id;
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_show_genres LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id WHERE tv_show_genres.genre_id >= 1 ORDER BY tv_shows.title, tv_show_genres.genre_id;
