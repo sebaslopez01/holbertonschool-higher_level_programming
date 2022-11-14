ddcf0070afb0c8ec6ccd228c12ddbce7259a43b8
Sebastian <sebaslopez.2002@gmail.com>
2022-11-14 09:15:50 -0800

    list shows and genres

---
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,2 @@
+-- List shows and genres
+SELECT tv_shows.title, tv_genres.name FROM tv_show_genres RIGHT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id RIGHT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id ORDER BY tv_shows.title, tv_genres.name;
