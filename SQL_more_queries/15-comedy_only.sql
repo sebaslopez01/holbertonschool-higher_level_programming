79c98845d6b100203a833c0f7b225aca53651712
Sebastian <sebaslopez.2002@gmail.com>
2022-11-14 09:13:24 -0800

    comedy only

---
+++ SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,2 @@
+-- Only comedy
+SELECT tv_shows.title FROM tv_show_genres LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id WHERE tv_genres.name = 'Comedy' ORDER BY tv_shows.title;
