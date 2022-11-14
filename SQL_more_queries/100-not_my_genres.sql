72aa2a58ecba81440bc1c0ff687f4d9fafaa9fb1
Sebastian <sebaslopez.2002@gmail.com>
2022-11-14 11:26:12 -0800

    not my genre

--- SQL_more_queries/100-not_my_genres.sql
+++ SQL_more_queries/100-not_my_genres.sql
@@ -1,3 +1,3 @@
 -- Not my genre
-SELECT tv_genres.name FROM tv_show_genres LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-WHERE tv_show_genres.show_id NOT IN (SELECT id FROM tv_shows WHERE title = 'Dexter') ORDER BY tv_genres.name;
+SELECT DISTINCT tv_genres.name FROM tv_show_genres LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_show_genres.genre_id NOT IN (SELECT tv_genres.id FROM tv_show_genres LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id WHERE tv_shows.title = 'Dexter') ORDER BY tv_genres.name;
