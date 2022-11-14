fa26b78d1273ea5b4fe9f54238662c208ceac2ee
Sebastian <sebaslopez.2002@gmail.com>
2022-11-14 08:47:26 -0800

    genre id all show

---
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,2 @@
+-- Genre ID for all shows
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_show_genres FULL OUTER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id ORDER BY tv_shows.title, tv_show.genres.genre_id;
