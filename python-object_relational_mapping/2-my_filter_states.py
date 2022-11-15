c77c68a25b8f210488ea1741419850d5504bad55
Sebastian <sebaslopez.2002@gmail.com>
2022-11-14 21:12:38 -0500

    added filter

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
     cur = db.cursor()
 
     cur.execute(
-        "SELECT * FROM states WHERE name = {} ORDER BY id".format(state_name))
+        "SELECT * FROM states WHERE name = '{}' ORDER BY id".format(state_name))
     rows = cur.fetchall()
 
     for row in rows:
