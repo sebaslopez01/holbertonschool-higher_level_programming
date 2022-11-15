82c1435a2cc49bb188d42c037673903d8b82fac7
Sebastian <sebaslopez.2002@gmail.com>
2022-11-14 22:34:23 -0500

    safe filter states

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""
+This script lists all values in the states where name matches the safe argument
+"""
+
+
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
+
+    username, password, database_name, state_name = sys.argv[1:]
+
+    db = MySQLdb.connect(host='localhost', user=username,
+                         passwd=password, db=database_name)
+    cur = db.cursor()
+
+    cur.execute("""
+        SELECT * FROM states
+        WHERE name LIKE BINARY %s
+        ORDER BY id
+    """, state_name)
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
