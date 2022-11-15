9d4ae58a827d569a4662dcdc5130e79f8772a6d0
Sebastian <sebaslopez.2002@gmail.com>
2022-11-14 22:36:39 -0500

    tuple added

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -19,7 +19,7 @@ if __name__ == '__main__':
         SELECT * FROM states
         WHERE name LIKE BINARY %s
         ORDER BY id
-    """, state_name)
+    """, (state_name,))
     rows = cur.fetchall()
 
     for row in rows:
