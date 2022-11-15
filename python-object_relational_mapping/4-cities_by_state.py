e7dbcdd5f23c59b588586da59dc5ef4fc3191873
Sebastian <sebaslopez.2002@gmail.com>
2022-11-14 22:41:31 -0500

    cities by state

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""
+This script lists all cities
+"""
+
+
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
+
+    username, password, database_name = sys.argv[1:]
+
+    db = MySQLdb.connect(host='localhost', user=username,
+                         passwd=password, db=database_name)
+    cur = db.cursor()
+
+    cur.execute("""
+        SELECT cities.id, cities.name, states.name
+        FROM cities
+        LEFT JOIN states
+        ON cities.state_id = states.id
+        ORDER BY cities.id
+    """)
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
