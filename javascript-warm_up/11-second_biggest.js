63ee9b73e7a60ca2b93cd2c4f2b98598c37ddcff
Sebastian <sebaslopez.2002@gmail.com>
2022-12-19 09:57:20 -0500

    second biggest

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const { argv } = require('process');
+
+if (argv.length === 2 || argv.length === 3) {
+  console.log('0');
+} else {
+  const arr = argv.slice(2).map((val) => parseInt(val));
+  arr.sort();
+  const lastVal = arr.pop();
+  const arr2 = arr.filter((val) => val !== lastVal);
+
+  console.log(arr2.pop());
+}
