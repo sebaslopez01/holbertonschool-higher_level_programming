8d1c314a2e7c1bde5fae7b651aae6d33bb519141
Sebastian <sebaslopez.2002@gmail.com>
2022-12-19 10:00:22 -0500

    fixed order

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -6,7 +6,7 @@ if (argv.length === 2 || argv.length === 3) {
   console.log('0');
 } else {
   const arr = argv.slice(2).map((val) => parseInt(val));
-  arr.sort();
+  arr.sort((a, b) => a - b);
   const lastVal = arr.pop();
   const arr2 = arr.filter((val) => val !== lastVal);
 
