f4a0f617b24b0433977bcc5ba9eead1055ce7d18
Sebastian <sebaslopez.2002@gmail.com>
2022-12-20 13:06:15 -0500

    fixed

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -5,22 +5,15 @@ const request = require('request');
 
 request(argv[2], (_error, _response, body) => {
   const data = JSON.parse(body);
-  const result = {
-    1: 0,
-    2: 0,
-    3: 0,
-    4: 0,
-    5: 0,
-    6: 0,
-    7: 0,
-    8: 0,
-    9: 0,
-    10: 0
-  };
+  const result = {};
 
   data.forEach((val) => {
     if (val.completed) {
-      result[val.userId] += 1;
+      if (result[val.userId]) {
+        result[val.userId] += 1;
+      } else {
+        result[val.userId] = 0;
+      }
     }
   });
 
